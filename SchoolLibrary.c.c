2e8cc94e0c78e77235a9261c8465a53341fd7e7a
stephenokeyo <90698968+stephenokeyo@users.noreply.github.com>
2021-10-26 14:44:44 +0300

    Add files via upload

---
+++ SchoolLibrary.c.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("\tSCHOOL LIBRARY\n");
+    printf("Welcome to the Library\n");
+    printf("What would you like to do in the Library?\n");
+    printf("1. New user? Enter your details to sign up.\n");
+    printf("2. Search for a book.\n");
+    printf("3. Explore the Library.\n");
+    printf("4. Borrow a book\n");
+    printf("5. Read a book in the Library without borrowing\n");
+    printf("6. Use research database\n");
+    printf("7. Attend or bring younger person to class\n");
+    printf("8. Borrow a DVD or a video tape of a movie\n");
+    printf("9. Get help from the Librarian\n");
+    printf("Please select one item.\n");
+    printf("Press any key to exit\n");
+    return 0;
+}
